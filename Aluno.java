12a333f9720e2aef02628d86a626322bf4cc346a
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-03-24 10:55:05 -0300

    Exercicio01

---
+++ Aluno.java
@@ -0,0 +1,8 @@
+public class Aluno {
+    
+  public String nome;
+  public double nota1;
+  public double nota2;
+  public double nota3;
+
+}
---
+++ Exercicio01.java
@@ -0,0 +1,19 @@
+/*Escrever um programa que leia o nome de um aluno e as notas das três provas que ele obteve no semestre. No final informar o nome do aluno e a sua média (aritmética) */
+public class Exercicio01 {
+    public static void executar(){
+   
+    Aluno aluno = new Aluno();
+
+    System.out.println("Informe os dados do Aluno: ");
+    aluno.nome = Prompt.lerLinha("Digite o nome do aluno");
+    aluno.nota1 = Prompt.lerDecimal("Digite a primeira nota: ");
+    aluno.nota2  = Prompt.lerDecimal("Digite a segunda nota: ");
+    aluno.nota3 = Prompt.lerDecimal("Digite a terceira nota: ");
+
+    double notaFinal = (aluno.nota1 + aluno.nota2 + aluno.nota3) / 3;
+
+        System.out.println("Nome: "+ aluno.nome);
+        System.out.println("Média: "+ notaFinal);
+}
+   
+}
