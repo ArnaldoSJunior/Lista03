c5bffd8e9024ccbb3e80a36d52c230aff816634c
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-03-31 15:17:30 -0300

    Exercicio16

---
+++ Exercicio16.java
@@ -0,0 +1,20 @@
+public class Exercicio16 {
+    public static void executar() {
+        
+        System.out.println("Digite o salário mínimo: ");
+        Funcionarios salario = new Funcionarios();
+        salario.salarioMinimo = Prompt.lerDecimal();
+        for (int i = 0; i < 584; i++) {
+            System.out.printf("Digite o salário do %d° funcionário: ", (i + 1));
+            salario.salarioInicial[i] = Prompt.lerDecimal();
+            salario.ajustar(i);
+
+        }
+        System.out.println("\n--- Salário Ajustado ---\n");
+        for (int i = 0; i < 584; i++) {
+            System.out.printf("Funcionario[%d]: Salario inicial: R$ %.2f  Salario final: R$ %.2f\n", (i + 1),
+                    salario.salarioInicial[i], salario.salarioFinal[i]);
+
+        }
+    }
+}
