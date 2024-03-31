10a55438d88e9abaa1a43f9e7a239eed56500f52
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-03-31 14:47:45 -0300

    Exercicio15

---
+++ Exercicio15.java
@@ -0,0 +1,28 @@
+/* A concessionária de veículos “CARANGO” está vendendo os seus veículos com desconto. Faça um programa que calcule e exiba o valor do desconto e o valor a ser pago pelo cliente. O desconto deverá ser calculado sobre o valor do veículo de acordo com o combustível (álcool – 25%, gasolina –21% ou diesel –14%). Com valor do veículo zero encerra entrada de dados. Informe total de desconto e total pago pelos clientes*/
+public class Exercicio15 {
+    public static void executar(){
+
+    Veiculos veiculo = new Veiculos();
+
+    do {
+        System.out.println("Informe o valor do veiculo: (Digite 0 para sair)");
+        veiculo.valorCarro=Prompt.lerDecimal();
+        if(veiculo.valorCarro != 0){ 
+        System.out.println("Qual o tipo de combustivel:");
+        veiculo.combustivel = Prompt.lerLinha();
+        veiculo.descontar();
+        
+       
+        System.out.println("Desconto aplicado: " +  veiculo.desconto);
+        System.out.println("Total pago:" + (veiculo.valorCarro -  veiculo.desconto));
+       
+    
+        }
+        
+    
+       }
+    while(veiculo.valorCarro != 0);
+
+  
+    }
+ } 
---
+++ Veiculos.java
@@ -0,0 +1,23 @@
+public class Veiculos {
+    public Double valorDesc;
+    public Double valorPgCliente;
+    public Double valorCarro;
+    public String combustivel;
+    public Double desconto;
+    public Double descFinal;
+
+
+    public void descontar() {
+      if(combustivel.equals("álcool")){
+        desconto = valorCarro * 0.25;
+      }else if(combustivel.equals("diesel")){
+        desconto = valorCarro * 0.14;
+      }else if(combustivel.equals("gasolina")){
+        desconto = valorCarro * 0.21;
+      }else{
+        System.out.println("Combustivel inválido!");
+      } 
+
+    }
+  
+}
