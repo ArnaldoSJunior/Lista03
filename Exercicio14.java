fb1248810c70343e9f11150cbdf8b0469329870a
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-03-31 13:30:11 -0300

    Exercicio14

---
+++ Exercicio14.java
@@ -0,0 +1,31 @@
+/*Faça um programa que receba o preço de custo e o preço de venda de 40 produtos. Mostre como resultado se houve lucro, prejuízo ou empate para cada produto. Informe média de preço de custo e do preço de venda. */
+public class Exercicio14 {
+    public static void executar(){
+
+
+     Lucratividade lucro = new Lucratividade();
+     System.out.println("---------Informe dados dos produtos--------");
+     
+    for(int i=0; i<40; i++)
+       {
+        System.out.println("Informe preço de custo produto"+(i+1)+":");
+        lucro.precoCusto[i]=Prompt.lerDecimal();
+        System.out.println("Informe preço de venda:");
+        lucro.precoVenda[i]=Prompt.lerDecimal();
+       }
+    for(int i=0; i<3; i++)
+       {
+        System.out.println("Produto"+(i+1));
+        lucro.verificar(i);
+       } 
+       double mediaPrecoCusto = lucro.calcularMediaPrecoCusto();
+       double mediaPrecoVenda = lucro.calcularMediaPrecoVenda();
+
+       System.out.println("Média de preço de custo: " + mediaPrecoCusto);
+       System.out.println("Média de preço de venda: " + mediaPrecoVenda);
+   
+
+
+
+    }
+}
---
+++ Lucratividade.java
@@ -0,0 +1,36 @@
+public class Lucratividade {
+
+  public Double[] precoCusto = new Double[40];
+  public Double[] precoVenda = new Double[40];
+ 
+ 
+
+
+public void verificar(int i){
+
+if(precoVenda[i] > precoCusto[i]){
+    System.out.println("Houve lucro!");
+}else if(precoCusto[i] > precoVenda[i]){
+    System.out.println("Houve prejuízo!");
+}else{
+    System.out.println("Houve empate!");
+}
+}
+
+public double calcularMediaPrecoCusto() {
+    double soma = 0;
+    for (double preco : precoCusto) {
+        soma += preco;
+    }
+    return soma / precoCusto.length;
+}
+
+public double calcularMediaPrecoVenda() {
+    double soma = 0;
+    for (double preco : precoVenda) {
+        soma += preco;
+    }
+    return soma / precoVenda.length;
+}
+
+}
